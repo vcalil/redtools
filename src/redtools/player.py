b08b94641d40f90ff59f402bc71a7e3bb17f0b19
Vinicius Calil <v.calil@hotmail.com>
2021-12-13 17:29:47 -0300

    Atualizando nomes dos arquivos, atualizando imports e classe player

--- src/redtools/player.py
+++ src/redtools/player.py
@@ -1,28 +1,27 @@
 import requests
+import json
 
 
 class RedPlayer:
 
 	def readJSON(rota):
 		with open(rota) as f:
-    		jsonFile = json.load(f)
-    	return jsonFile
+			jsonFile = json.load(f)
 
-    def getRequest(url, headers, data, variables):
+		return jsonFile
+
+	def getRequest(url, headers, data, variables):
 		response = requests.get(url, headers=headers, data=data, verify=False)
-
 		jsonResponseContent = json.loads(response.content)
-		myVariable = jsonResponseContent[variable]
-
-		return myVariable
-
+		for count in range(variables):
+			myVariables[count] = jsonResponseContent[variables[count]]
+			
+		return myVariables
 
 	def postRequest(url, headers, data, variables):
 		response = requests.post(url, headers=headers, data=data, verify=False)
-
 		jsonResponseContent = json.loads(response.content)
-		for in range(variables):
-			myVariable
-		myVariable = jsonResponseContent[variable]
-
-		return myVariable
+		for count in range(variables):
+			myVariables[count] = jsonResponseContent[variables[count]]
+		
+		return myVariables
