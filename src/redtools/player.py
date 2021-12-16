07e3240d0122b67251c3bd1d84c1ceec2fd6a836
Vinicius Calil <v.calil@hotmail.com>
2021-12-16 12:39:35 -0300

    adicionando variavel self

--- src/redtools/player.py
+++ src/redtools/player.py
@@ -5,14 +5,14 @@ import json
 class RedPlayer:
 	def __init__(self):
     		self.data = []
-    		
-	def readJSON(rota):
+
+	def readJSON(self, rota):
 		with open(rota) as f:
 			jsonFile = json.load(f)
 
 		return jsonFile
 
-	def getRequest(url, headers, data, variables):
+	def getRequest(self, url, headers, data, variables):
 		response = requests.get(url, headers=headers, data=data, verify=False)
 		jsonResponseContent = json.loads(response.content)
 		for count in range(variables):
@@ -20,7 +20,7 @@ class RedPlayer:
 			
 		return myVariables
 
-	def postRequest(url, headers, data, variables):
+	def postRequest(self, url, headers, data, variables):
 		response = requests.post(url, headers=headers, data=data, verify=False)
 		jsonResponseContent = json.loads(response.content)
 		for count in range(variables):
